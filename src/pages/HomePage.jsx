09e4b4a0ccc163492057f561b046fcc54927a9bb
geromansur0-a11y <geromansur0@gmail.com>
2025-12-30 12:45:33 +0700

    Create HomePage.jsx

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Header from '../components/Header'
+import Hero from '../components/Hero'
+import Features from '../components/Features'
+import Testimonials from '../components/Testimonials'
+import Footer from '../components/Footer'
+
+const HomePage = () => {
+  return (
+    <div>
+      <Header />
+      <Hero />
+      <Features />
+      <Testimonials />
+      <Footer />
+    </div>
+  )
+}
+
+export default HomePage
