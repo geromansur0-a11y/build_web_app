e0d51d986822b158a42948153891cd45ed0d49e8
geromansur0-a11y <geromansur0@gmail.com>
2025-12-30 13:11:38 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/hostinger-horizons-app/', // Nama repository Anda
   server: {
     port: 5173,
     host: true
